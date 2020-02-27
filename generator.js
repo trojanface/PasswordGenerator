15df09283f481d4fd798612c7617eee21f6676e6
MattT <trojanface@hotmail.com>
2020-02-27 12:52:40 +1030

    Initial commit

---
+++ generator.js
@@ -0,0 +1,4 @@
+//button that when pressed will open options for your password generation
+//options such as password length, capitals?, numbers? special characters?
+//button that generates the password.
+//algorithm that feeds the criteria and turns it into a random generator.
