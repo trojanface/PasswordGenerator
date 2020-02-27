b6491186b80b0f589a702905726f46e61fad3e47
MattT <trojanface@hotmail.com>
2020-02-27 12:54:57 +1030

    Copy and pasted code into the correct files in the git repo. Created a basic html page with basic bootstrap elements. Created a list of variables in the javascript file.

--- generator.js
+++ generator.js
@@ -2,3 +2,15 @@
 //options such as password length, capitals?, numbers? special characters?
 //button that generates the password.
 //algorithm that feeds the criteria and turns it into a random generator.
+
+
+//User options
+let password_length = 3;
+let use_Capitals = true;
+let use_Special = true; 
+
+//Stored Data
+
+let letter_array = "abcdefghijklmnopqrstuvwxyz";
+let number_array = "0123456789";
+let special_array = ["\~","\!","\@","\#","\$","\%","\^","\&","\*"];
