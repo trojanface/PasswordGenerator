2d8e2b45377ec8d2654b774cd54ca82332c1b871
MattT <trojanface@hotmail.com>
2020-02-27 13:28:48 +1030

    Created interactivity between capital number selection and corresponding javascript variables

--- generator.js
+++ generator.js
@@ -6,11 +6,19 @@
 
 //User options
 let password_length = 3;
-let use_Capitals = true;
-let use_Special = true; 
+let capital_number = "0";
+let special_number = 0; 
 
 //Stored Data
 
 let letter_array = "abcdefghijklmnopqrstuvwxyz";
 let number_array = "0123456789";
 let special_array = ["\~","\!","\@","\#","\$","\%","\^","\&","\*"];
+
+
+
+function capitalfunc(countVar) {
+    document.getElementById("capital_button").innerHTML = ""+countVar;
+    capital_number = countVar;
+}
+
