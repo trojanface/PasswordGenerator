8bb45c0576821d624dd6977520c20e63fde708ef
MattT <trojanface@hotmail.com>
2020-03-02 13:02:46 +1030

    Added CSS styling and a regenerate button.

--- generator.js
+++ generator.js
@@ -1,5 +1,5 @@
 //button that when pressed will open options for your password generation
-//options such as password length, capitals?, numbers? special characters?
+//options suc as password length, capitals?, numbers? special characters?
 //button that generates the password.
 //algorithm that feeds the criteria and turns it into a random generator.
 
@@ -7,13 +7,13 @@
 //User options
 let password_length = 6;
 let capital_number = 2;
-let special_number = 1; 
+let special_number = 1;
 
 //Stored Data
 
 let letter_array = "abcdefghijklmnopqrstuvwxyz";
 let number_array = "0123456789";
-let special_array = ["\~","\!","\@","\#","\$","\%","\^","\&","\*"];
+let special_array = ["\~", "\!", "\@", "\#", "\$", "\%", "\^", "\&", "\*"];
 
 //Password
 let password = "";
@@ -22,32 +22,37 @@ let password = "";
 function selectionfunc(countVar, optionchoice) {
     if (optionchoice === "Capital") {
         capital_number = countVar;
-        document.getElementById("capital_button").innerHTML = ""+countVar;
+        document.getElementById("capital_button").innerHTML = "" + countVar;
     } else if (optionchoice === "Special") {
         special_number = countVar;
-        document.getElementById("special_button").innerHTML = ""+countVar;
+        document.getElementById("special_button").innerHTML = "" + countVar;
     } else {
         password_length = countVar;
-        document.getElementById("length_button").innerHTML = ""+countVar;
+        document.getElementById("length_button").innerHTML = "" + countVar;
     }
 }
 
 //Password Generation
 function generate() {
-password = ""; //Clears the password variable.
-for (let j = 0; j < capital_number; j++) {
-    password += letter_array[Math.floor(Math.random()*letter_array.length)].toUpperCase();
-}
-for (let k = 0; k < special_number; k++) {
-    password += special_array[Math.floor(Math.random()*special_array.length)];
-}
-for (let i = 0; i < password_length-(special_number+capital_number); i++) {
-    password += letter_array[Math.floor(Math.random()*letter_array.length)];
-}
-for (let l = 0; l < 300; l++){
-    let random_selection = Math.floor(Math.random()*password.length);
-    password += password[random_selection];
-    password = password.substring(0, random_selection) + password.substring(random_selection + 1);
-}
-console.log(""+password);
+    var regen_button_var = document.getElementById("refresh_button");
+    if (regen_button_var.style.display === "none") {
+        regen_button_var.style.display = "block";
+    } 
+    password = ""; //Clears the password variable.
+    document.getElementById("start_button").innerHTML = "Options";
+    for (let j = 0; j < capital_number; j++) { //adds capital letters
+        password += letter_array[Math.floor(Math.random() * letter_array.length)].toUpperCase();
+    }
+    for (let k = 0; k < special_number; k++) { //adds special characters
+        password += special_array[Math.floor(Math.random() * special_array.length)];
+    }
+    for (let i = 0; i < password_length - (special_number + capital_number); i++) { //adds lowercase letters
+        password += letter_array[Math.floor(Math.random() * letter_array.length)];
+    }
+    for (let l = 0; l < 300; l++) { //scrambles the order
+        let random_selection = Math.floor(Math.random() * password.length);
+        password += password[random_selection];
+        password = password.substring(0, random_selection) + password.substring(random_selection + 1);
+    }
+    document.getElementById("password_text").innerHTML = password;
 }
