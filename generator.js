a8d19010d514a0c35258d6d77d6a17d1831e7299
MattT <trojanface@hotmail.com>
2020-03-03 20:20:50 +1030

    Now changing the length option to a menu for lowercase characters. The password length will be determined by how many you choose from each menu. This will be read and displayed in the modal.

--- generator.js
+++ generator.js
@@ -8,6 +8,7 @@
 let password_length = 6;
 let capital_number = 2;
 let special_number = 1;
+let number_number = 2;
 
 //Stored Data
 
@@ -26,6 +27,9 @@ function selectionfunc(countVar, optionchoice) {
     } else if (optionchoice === "Special") {
         special_number = countVar;
         document.getElementById("special_button").innerHTML = "" + countVar;
+    } else if (optionchoice === "Number") {
+        number_number = countVar;
+        document.getElementById("number_button").innerHTML = "" + countVar;
     } else {
         password_length = countVar;
         document.getElementById("length_button").innerHTML = "" + countVar;
@@ -40,10 +44,13 @@ function generate() {
     for (let j = 0; j < capital_number; j++) { //adds capital letters
         password += letter_array[Math.floor(Math.random() * letter_array.length)].toUpperCase();
     }
+    for (let m = 0; m < number_number; m++) { //adds number
+        password += number_array[Math.floor(Math.random() * number_array.length)];
+    }
     for (let k = 0; k < special_number; k++) { //adds special characters
         password += special_array[Math.floor(Math.random() * special_array.length)];
     }
-    for (let i = 0; i < password_length - (special_number + capital_number); i++) { //adds lowercase letters
+    for (let i = 0; i < password_length - (number_number + special_number + capital_number); i++) { //adds lowercase letters
         password += letter_array[Math.floor(Math.random() * letter_array.length)];
     }
     for (let l = 0; l < 300; l++) { //scrambles the order
