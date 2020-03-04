dc302ddbf99834994c81c3f2b174d8ff4cde4a97
MattT <trojanface@hotmail.com>
2020-03-04 21:45:03 +1030

    Deleting old code and adding new boolean variables

--- generator.js
+++ generator.js
@@ -11,6 +11,12 @@ let capital_number = 2;
 let special_number = 1;
 let number_number = 1;
 
+let use_lowercase = true;
+let use_uppercase = false;
+let use_special = false;
+let use_number = false;
+
+
 //Stored Data
 
 let letter_array = "abcdefghijklmnopqrstuvwxyz";
@@ -28,55 +34,17 @@ let number_text_variable = document.getElementById("number_text");
 let length_text_variable = document.getElementById("password_length_text");
 let warning_text = document.getElementById("invalid_warning");
 let generate_button_variable = document.getElementById("generate_button");
-
-//need to declare these html elements as variables to prevent doing the getelement call.
+let password_text_variable = document.getElementById("password_text");
+let refresh_button = document.getElementById("refresh_button");
+let start_button = document.getElementById("start_button");
 //add dark mode
 
-//User Input function
-function selectionfunc(countVar, optionchoice) {
-    if (optionchoice === "Uppercase") {
-        capital_number = less_than_zero(capital_number, countVar);
-        uppercase_text_variable.innerHTML = "" + capital_number;
-    } else if (optionchoice === "Special") {
-        special_number = less_than_zero(special_number, countVar);
-        special_text_variable.innerHTML = "" + special_number;
-    } else if (optionchoice === "Number") {
-        number_number = less_than_zero(number_number, countVar);
-        number_text_variable.innerHTML = "" + number_number;
-    } else {
-        lowercase_number = less_than_zero(lowercase_number, countVar);
-        lowercase_text_variable.innerHTML = "" + lowercase_number;
-        
-    }
-    password_length = lowercase_number + capital_number +special_number + number_number;
-    lowercase_text_variable.innerHTML = "" + password_length;
-    if (password_length < 8) {
-        warning_text.style.display = "inline";
-        generate_button_variable.style.display = "none";
-    } else {
-        generate_button_variable.style.display = "inline";
-        warning_text.style.display = "none";
-    }
-}
-
-//Protect against going below zero function
-function less_than_zero (current,change) {
-    if (change < 0) {
-        if (current > 0) {
-        return current += change;
-        } else {
-           return current;
-        }
-    } else {
-        return current += change;
-    }
-   }
 
 //Password Generation
 function generate() {
-    document.getElementById("refresh_button").style.display = "inline"; //makes the regenerate button visible
+    refresh_button.style.display = "inline"; //makes the regenerate button visible
     password = ""; //Clears the password variable.
-    document.getElementById("start_button").innerHTML = "Options"; //changes the text of the start button to options
+    start_button.innerHTML = "Options"; //changes the text of the start button to options
     for (let j = 0; j < capital_number; j++) { //adds capital letters
         password += letter_array[Math.floor(Math.random() * letter_array.length)].toUpperCase();
     }
@@ -94,5 +62,5 @@ function generate() {
         password += password[random_selection];
         password = password.substring(0, random_selection) + password.substring(random_selection + 1);
     }
-    document.getElementById("password_text").innerHTML = password;
+    password_text_variable.innerHTML = password;
 }
