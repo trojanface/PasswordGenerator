c4748029e64508feb0431e88ff909392da919f06
MattT <trojanface@hotmail.com>
2020-03-04 20:37:40 +1030

    Changing the + and - buttons to checkboxes and the password length to a slider. This is to better align with assignment requirements.

--- generator.js
+++ generator.js
@@ -20,32 +20,42 @@ let special_array = ["\~", "\!", "\@", "\#", "\$", "\%", "\^", "\&", "\*"];
 //Password
 let password = "";
 
+//HTML Element variables - to preserve performance.
+let uppercase_text_variable =  document.getElementById("uppercase_text");
+let lowercase_text_variable = document.getElementById("lowercase_text");
+let special_text_variable = document.getElementById("special_text");
+let number_text_variable = document.getElementById("number_text");
+let length_text_variable = document.getElementById("password_length_text");
+let warning_text = document.getElementById("invalid_warning");
+let generate_button_variable = document.getElementById("generate_button");
 
+//need to declare these html elements as variables to prevent doing the getelement call.
+//add dark mode
 
 //User Input function
 function selectionfunc(countVar, optionchoice) {
     if (optionchoice === "Uppercase") {
         capital_number = less_than_zero(capital_number, countVar);
-        document.getElementById("uppercase_text").innerHTML = "" + capital_number;
+        uppercase_text_variable.innerHTML = "" + capital_number;
     } else if (optionchoice === "Special") {
         special_number = less_than_zero(special_number, countVar);
-        document.getElementById("special_text").innerHTML = "" + special_number;
+        special_text_variable.innerHTML = "" + special_number;
     } else if (optionchoice === "Number") {
         number_number = less_than_zero(number_number, countVar);
-        document.getElementById("number_text").innerHTML = "" + number_number;
+        number_text_variable.innerHTML = "" + number_number;
     } else {
         lowercase_number = less_than_zero(lowercase_number, countVar);
-        document.getElementById("lowercase_text").innerHTML = "" + lowercase_number;
+        lowercase_text_variable.innerHTML = "" + lowercase_number;
         
     }
     password_length = lowercase_number + capital_number +special_number + number_number;
-    document.getElementById("password_length_text").innerHTML = "" + password_length;
+    lowercase_text_variable.innerHTML = "" + password_length;
     if (password_length < 8) {
-        document.getElementById("invalid_warning").style.display = "inline";
-        document.getElementById("generate_button").style.display = "none";
+        warning_text.style.display = "inline";
+        generate_button_variable.style.display = "none";
     } else {
-        document.getElementById("generate_button").style.display = "inline";
-        document.getElementById("invalid_warning").style.display = "none";
+        generate_button_variable.style.display = "inline";
+        warning_text.style.display = "none";
     }
 }
 
